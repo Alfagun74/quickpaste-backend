95c931b8013df96d6c20667369b844aecd4a8c54
Alper Alkan <alper.alkan2000@gmail.com>
2021-02-21 04:04:34 +0100

    sokcet io express variant

--- src/server/Quickpaste-Server.ts
+++ src/server/Quickpaste-Server.ts
@@ -1,50 +1,57 @@
-import { createServer, Socket } from "net";
 import { Logger } from "tslog";
 import { nanoid } from "nanoid";
 import prettyBytes from "pretty-bytes";
+import express from "express";
+import http from "http"
+import { Server, Socket } from "socket.io";
+import { start } from "repl";
 
 const log = new Logger();
 const port = 80;
-const server = createServer();
 const sockets: Map<string, Socket> = new Map();
 
-server.listen(port, () => {
-  log.info(`Server listening for connection requests on socket localhost:${port}`);
-});
+function startServer() {
+  const app = express();
+  const server = http.createServer(app);
+  const io = new Server(server);
+  // example on how to serve a simple API
+  //app.get("/random", (req, res) => res.send(generateRandomNumber()));
+  // example on how to serve static files from a given folder
+  //app.use(express.static("public"));
 
-server.on('error', (error: Error) => {
-  log.fatal(`ERROR on Server`, error.message);
-});
+  io.on("connection", onNewWebsocketConnection);
+  server.listen(port, () => console.info(`Server listening for connection requests on socket localhost: ${port}`));
+  server.on('error', (error: Error) => {
+    log.error(`ERROR on server`, error.message);
+  });
+}
 
-server.on('connection', (socket: Socket) => {
-  socket.setDefaultEncoding("utf-8");
-  socket.setEncoding("utf-8");
-  socket.setKeepAlive(true);
-  const socketId = nanoid();
-  sockets.set(socketId, socket);
-  log.info(`Client ${socketId} connected from ${socket.remoteAddress}:${socket.remotePort}`);
+function onNewWebsocketConnection(socket: Socket) {
+  sockets.set(socket.id, socket);
+  log.info(`Client ${socket.id} connected from ${socket.handshake.address}`);
 
-  socket.on('data', (data: Buffer) => {
-    const dataLength = prettyBytes(data.length ?? 0, { locale: 'de' })
-    log.info(`Datapacket (${dataLength}) received from client.`);
+  socket.on('data', (data: any) => {
+    log.info(`Datapacket received from client ${socket.id}`, data);
     if (sockets.size <= 1) {
-      log.info(`Client ${socketId} is alone.`);
+      log.info(`Client ${socket.id} is alone.`);
       return;
     }
     sockets.forEach((socket: Socket, id: string) => {
-      if (id != socketId) {
-        socket.write(data);
-        log.info(`Datapacket(${dataLength}) sent from client ${socketId} to client ${id}.`);
+      if (id != socket.id) {
+        socket.emit("data", data);
+        log.info(`Datapacket sent from client ${socket.id} to client ${id}.`);
       }
     });
   });
 
   socket.on('end', function () {
-    log.info(`Closing connection with client ${socketId} `);
-    sockets.delete(socketId);
+    log.info(`Closing connection with client ${socket.id}`);
+    sockets.delete(socket.id);
   });
 
   socket.on('error', (error: Error) => {
-    log.error(`ERROR on client: ${socketId}, ${socket.remoteAddress}: ${socket.remotePort} `, error.message);
+    log.error(`ERROR on client: ${socket.id}, ${socket.handshake.address} `, error.message);
   });
-});
+}
+
+startServer();
