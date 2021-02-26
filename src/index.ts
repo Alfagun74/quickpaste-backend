48ad0bfb8d7e98dbefca694da37439ae6d69bb77
Alper Alkan <alper.alkan2000@gmail.com>
2021-02-26 16:00:28 +0100

    bester server

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,36 @@
 import database from "./database";
 import WebsocketHandler from "./websockethandler";
-import fastify from "fastify";
 import { Server } from "socket.io";
 import { Logger } from "tslog";
+import { QuickpasteModel } from "./models/quickpaste.model";
+import express, { Request, Response } from "express";
 
 const log = new Logger();
 const port = process.env.PORT ?? 80;
-const app = fastify();
+const app = express();
 
 if (process.env.NODE_ENV === "prod") {
     database(process.env.DB_HOST ?? "");
-    //app.get("/", (request, reply) => {
-    //     reply.status(200);
-    //});
+    app.get("/last/:roomcode", async (request: Request, response: Response) => {
+        const roomCode: string = request.params.roomcode;
+        const quickpastes = await QuickpasteModel.find(
+            { room: roomCode },
+            null,
+            {
+                limit: 10,
+                sort: { _createdAt: -1 },
+            }
+        );
+        response.json(quickpastes).status(200);
+    });
 }
 
-app.listen({ port: +port, host: "0.0.0.0" }, (err: Error, address: string) => {
-    if (err) throw err;
+const server = app.listen({ port: +port, host: "0.0.0.0" }, () => {
     new WebsocketHandler(
-        new Server(app.server, {
+        new Server(server, {
             pingTimeout: 15 * 60 * 10000,
             maxHttpBufferSize: 1e600,
         })
     );
-    log.info(
-        `Server listening for connection requests on socket localhost: ${address}`
-    );
+    log.info(`Server listening for connection requests on port ${port}`);
 });
