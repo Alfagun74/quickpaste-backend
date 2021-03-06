2199694f8315a1ba96d9d6099e165836c09749c8
Alper Alkan <alper.alkan2000@gmail.com>
2021-03-06 17:30:30 +0100

    sf

--- src/index.ts
+++ src/index.ts
@@ -37,6 +37,7 @@ if (process.env.NODE_ENV === "prod") {
             if (!decryptedData) {
                 throw Error("Error decrypting file");
             }
+            console.log(decryptedData.toString(ɵn));
             quickpaste.img = decryptedData.toString(ɵn);
             console.log(quickpaste);
         }
